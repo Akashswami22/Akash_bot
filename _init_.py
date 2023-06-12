e29cb44678caec806c4e8c249efd124855df0dbf
Akashswami22 <136350014+Akashswami22@users.noreply.github.com>
2023-06-12 21:18:18 +0530

    Add files via upload

---
+++ _init_.py
@@ -0,0 +1,14 @@
+from telethon import TelegramClient
+import logging
+import time 
+
+
+openai_key = "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"
+api_id = "1125689"
+
+api_hash = "4772d1792ed194020a8fb06a91ffb8fa"
+
+bot_token"5997196046:AAHrwh9gG-NonVme5q4EzMNz421lDOBDlbw"
+
+bot = TelegramClient("Aakash", api_id, api_hash
+   ).start(bot_token = bot_token)
